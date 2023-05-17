471fe1263cd7101891fae30ba1f83c664432582b
Hatim Margi <hatimmargi4@gmail.com>
2023-05-18 00:13:31 +0200

    Update for babyboo

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function Joke({ setup, punchline, num }) {
-  const [isShown, setIsShown] = React.useState(false)
+  const [isShown, setIsShown] = React.useState(false);
 
-  function toggleShown () {
-    setIsShown (preValue => !preValue)
+  function toggleShown() {
+    setIsShown((preValue) => !preValue);
   }
   return (
     // <div className="joke">
@@ -14,14 +14,30 @@ export default function Joke({ setup, punchline, num }) {
     //   <button onClick={toggleShown}>{isShown ? 'Hide' : 'Show'}  Joke</button>
     // </div>
     <div className="container">
-<div className="hbd">
-<h1>Happy Birthday Suha!</h1>
-      {isShown && <p>Let the rain come down and wash away your tears ❤️ <br/> 
-Let it fill your soul and drown your fears ❤️ <br/> 
-Let it shatter the walls for a new sun ❤️</p>}
-      <button onClick={toggleShown}>{isShown ? 'Hide' : 'Click Here ❤️'}</button>
-
-</div>
+      <div className="hbd">
+        <h1> ❤️كل عام وأنتي بخير حبيبتي أبرار</h1>
+        {isShown && (
+          <div>
+            <p>
+              وكان ET بالعربى قد تواصل بعد الحادث مع دانا حمدان، شقيقة مى سليم،
+              لكشف ملابسات الواقعة، والاطمئنان على الحالة الصحية لميّ، وقالت في
+              وقتها: "مى دلوقتي الحمد لله فى منزلها، وهي بخير، وكانت حادثة قوية
+              تعرضت لها أثناء توجهها للوكشين التصوير، حيث اصطدمت سيارتين
+              بسيارتها، وهى بخير، لكن الصدمة كانت قوية، والحمد لله قدّر ولطف"،
+              مطالبة الجمهور بالدعاء لها.❤️
+            </p>
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Backgrounds_and_Borders/Resizing_background_images"
+              target="_blank"
+              id="heart"
+              class="button"
+            ></a>{" "}
+          </div>
+        )}
+        <button onClick={toggleShown}>
+          {isShown ? "أخفي" : "أضغط ❤️"}
+        </button>
+      </div>
     </div>
   );
 }
